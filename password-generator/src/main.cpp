eb05264ee6141c1d37d8008c5277d64633c1f733
leggen-assis <souzaferreira437@gmail.com>
2024-10-04 09:59:25 -0300

    'password-generator' folder was removed

--- password-generator/src/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "Files/password_generator.cpp"
-
-int main(int argc, char* argv[]) {
-    std::cout << "Sua senha: " << generatePassword() << std::endl;
-}
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "password_generator.hpp"
+
+int main(void) {
+    std::cout << "Sua senha: " << generatePassword() << std::endl;
+}
